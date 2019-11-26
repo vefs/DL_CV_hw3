402aaf5a1e4c2b52c4bdfa96aaa305437c7ce6f3
vefs <weichen.vefs@gmail.com>
2019-11-26 22:28:16 +0800

    Add files via upload

---
+++ data/digit_mini/train.f
@@ -0,0 +1,10 @@
+./data/digit_mini/images/train/1.png
+./data/digit_mini/images/train/2.png
+./data/digit_mini/images/train/3.png
+./data/digit_mini/images/train/4.png
+./data/digit_mini/images/train/5.png
+./data/digit_mini/images/train/6.png
+./data/digit_mini/images/train/7.png
+./data/digit_mini/images/train/8.png
+./data/digit_mini/images/train/9.png
+./data/digit_mini/images/train/10.png
